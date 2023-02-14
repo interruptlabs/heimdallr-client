849b0b0537c51edb7dfa1f0259ffe1c4c8f30703
Robert S <122106553+RobertNotRob@users.noreply.github.com>
2023-02-14 22:25:53 +0000

    Inital test version

---
+++ universal-stub/forge.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "packagerConfig": {
+    "protocols": [
+      {
+        "name": "IDA Heimdallr Deeplink",
+        "schemes": ["ida-test"]
+      }
+    ]
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      "config": {
+        "mimeType": ["x-scheme-handler/ida-test"]
+      }
+    }
+  ],
+};
