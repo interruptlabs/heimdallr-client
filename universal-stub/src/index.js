2e488f8a86561bff9ae388128beb38b8c63af713
Robert S <122106553+RobertNotRob@users.noreply.github.com>
2023-02-15 19:28:44 +0000

    More comments

--- universal-stub/src/index.js
+++ universal-stub/src/index.js
@@ -6,6 +6,7 @@ const path = require('path')
 // Stop GPU process, optimise start speed
 app.disableHardwareAcceleration()
 
+// Load config for heimdallr_client
 if (process.platform == "win32") {
   config_path = path.join(process.env.APPDATA, "heimdallr/settings.json")
 }else{
@@ -16,17 +17,16 @@ console.log(config_path)
 const fs = require('fs')
 let rawdata = fs.readFileSync(config_path)
 let config = JSON.parse(rawdata)
-
 console.log(config)
 
 if(!config.hasOwnProperty('heimdallr_client')){
   dialog.showErrorBox("Heidmallr Client setting not set!")
   app.exit(1)
 }
-
 heimdallr_client = config.heimdallr_client
 console.log(heimdallr_client)
 
+// Register URL handlers
 if (process.defaultApp) {
   if (process.argv.length >= 2) {
     app.setAsDefaultProtocolClient('ida')
@@ -37,11 +37,11 @@ if (process.defaultApp) {
 }
 
 function heimdallrURL(url){
-
+  // Spawn Python
   pyProc = require('child_process').spawnSync(heimdallr_client, [url], {shell: true})
   // dialog.showErrorBox('URL Done', url)
 
-
+  // Check output
   if (pyProc != null) {
     console.log('child process success')
     console.log(pyProc.stderr.toString())
@@ -56,17 +56,18 @@ function heimdallrURL(url){
   }
 }
 
-
+// timeout after 500ms
 const gotTheLock = app.requestSingleInstanceLock()
 exit_timeout = setTimeout(function(){ app.exit(0) }, 500);
 if (!gotTheLock) {
   app.quit()
 } else {
+  // Windows
   app.on('second-instance', (event, commandLine, workingDirectory) => {
     clearTimeout(exit_timeout)
     heimdallrURL(`${commandLine.pop().slice(0,-1)}`)
   })
-  
+  // Linux/Darwin
   app.on('open-url', (event, url) => {
     clearTimeout(exit_timeout)
     heimdallrURL(`${url}`)
